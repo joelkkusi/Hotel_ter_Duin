526fd367fd2248523a2ec971780cfd05b721f34f
Noah <nannu2708@hotmail.com>
2023-06-02 12:15:12 +0200

    test

--- database/migrations/2023_06_02_095910_create_klant_table.php
+++ database/migrations/2023_06_02_095910_create_klant_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
 
             $table->string("nameee");
             $table->string("username");
-            $table->string("password");
+            $table->string("passwor");
 
             $table->timestamps();
         });
