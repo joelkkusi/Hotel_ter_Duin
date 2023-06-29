8885a36cff08ad20bb614a1b531bb52900da0f72
EmreCop <99261888+EmreCop@users.noreply.github.com>
2023-06-30 00:26:59 +0200

    aaaaaaaaaa

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -1,27 +1,26 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <title>Room Reservation Confirmation</title>
     <!-- Add your CSS stylesheets and scripts here if needed -->
 </head>
+
 <body>
     <div id="reservation-confirmation">
         <h2>Your Room Reservation</h2>
         <p>Your room has been reserved successfully!</p>
-        
+
         <h3>Invoice</h3>
-        <!-- Replace the placeholder values with the actual room information retrieved from the database -->
-<<<<<<< HEAD
-        <p>Room Number: $room-></p>
-=======
-        <!-- <p>Room Number: {{roomNumber}}</p>
->>>>>>> 5cfb6c2089e7dcf28f98fc192c3ababbeb28a5f6
+        <!-- Replace the placeholder values with the actual room information retrieved from the database 
+        <p>Room Number: {{roomNumber}}</p> 
         <p>Check-in Date: {{checkInDate}}</p>
         <p>Check-out Date: {{checkOutDate}}</p>
         <p>Total Amount: {{totalAmount}}</p> -->
-        
+
         <!-- You can add additional details to the invoice as needed -->
     </div>
     <!-- Add your JavaScript code here if needed -->
 </body>
+
 </html>
